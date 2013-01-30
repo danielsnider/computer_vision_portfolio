4f7020f96d8b80a7f8d3519abaf579642fa1b039
Daniel Snider <danielsnider12@gmail.com>
2013-01-30 01:59:19 +0100

    binary build script

---
+++ binaries/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gcc ./source_files/empty_wall_space/empty_wall_space.c -o ./empty_wall_space.out `pkg-config --cflags opencv` `pkg-config --libs opencv`
+g++ -g ./source_files/contour_find/contour_find.cpp -o ./contour_find.out `pkg-config --cflags opencv` `pkg-config --libs opencv`
+g++ -g ./source_files/highlight_ocr/MyFuncs.cpp ./source_files/highlight_ocr/main.cpp -o ./highlight_ocr.out `pkg-config --cflags opencv` `pkg-config --libs opencv`
+g++ -g ./source_files/quiz_it/MyImage.cpp ./source_files/quiz_it/MyFuncs.cpp ./source_files/quiz_it/main.cpp -o ./quizit.out `pkg-config --cflags opencv` `pkg-config --libs opencv`
+
