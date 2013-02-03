53bb488bd811673e7eb835980e1b41dedd2514a6
Daniel Snider <danielsnider12@gmail.com>
2013-02-03 08:08:22 +0100

    updated resume and contourfind slideshow

--- public/ws/engine5/script.js
+++ public/ws/engine5/script.js
@@ -18,4 +18,4 @@ function ws_fade(c,a,b){var e=jQuery;var d=e("ul",b);var f={position:"absolute",
 // Obfuscated by Javascript Obfuscator
 // http://javascript-source.com
 //***********************************************
-jQuery("#wowslider-container3").wowSlider({effect:"fade",prev:"",next:"",duration:20*100,delay:30*100,width:480,height:360,autoPlay:true,stopOnHover:false,loop:false,bullets:1,caption:false,captionEffect:"slide",controls:true,logo:"engine3/loading.gif",onBeforeStep:0,images:0});
+jQuery("#wowslider-container5").wowSlider({effect:"fade",prev:"",next:"",duration:20*100,delay:30*100,width:360,height:440,autoPlay:true,stopOnHover:false,loop:false,bullets:true,caption:false,captionEffect:"slide",controls:true,logo:"engine5/loading.gif",onBeforeStep:0,images:0});
